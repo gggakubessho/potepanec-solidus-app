d1cbc2f2e51894f30fb6f7026c253bda5afbfdc3
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-04-12 10:03:55 +0900

    split product/show into light & maincontent section

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,5 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find_by(id: params[:id])
-    @master_images = @product.master_images
   end
 end
