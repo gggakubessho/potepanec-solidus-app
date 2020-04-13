1611f5c49c6719301b9999ccf92052c0a4266414
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-04-14 00:38:28 +0900

    fix first code review issues

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe "Title" do
-    it "#full_title" do
+  describe "#full_title" do
+    it "displayed full title" do
       expect(full_title(page_title: "test")).to eq "test - BIGBAG Store"
       expect(full_title(page_title: "")).to eq "BIGBAG Store"
     end
