95f84ae5bf1283fbe48772aecbd09f15077baeed
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-04-22 08:11:55 +0900

    fix N+1 problem in categories_controller

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,7 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
-    @taxonomies = Spree::Taxonomy.all
+    @taxonomies = Spree::Taxonomy.all.includes(:root)
     @taxon = Spree::Taxon.find(params[:id])
-    @products = @taxon.all_products
+    @products = @taxon.all_products.includes(master: [:images,:default_price])
   end
 end
