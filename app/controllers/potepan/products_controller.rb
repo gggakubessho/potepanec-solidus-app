6be65dc1720af710df7543dbdec0cac96c3a233e
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-04-26 19:42:16 +0900

    add controller&view code for related_products

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,9 @@
 class Potepan::ProductsController < ApplicationController
+  MAX_RELATED_PRODUCTS = 4
+
   def show
     @product = Spree::Product.find(params[:id])
+    @related_products = Spree::Product.in_taxons(@product.taxons.ids).
+      distinct.where.not(id: @product.id).limit(MAX_RELATED_PRODUCTS)
   end
 end
