3ca6c3a922078ce9fc1757452434783bd8a32909
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-04-28 09:25:25 +0900

    add request spec for related_products

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -24,5 +24,15 @@ RSpec.describe "Products", type: :request do
     it '商品金額が含まれていること' do
       expect(response.body).to include "23.45"
     end
+
+    it "同一カテゴリの商品が関連する商品として含まれていること" do
+      expect(response.body).to include "Related Product"
+      expect(response.body).to include "99.99"
+    end
+
+    it "異なるカテゴリの商品が関連しない商品として含まれていないこと" do
+      expect(response.body).not_to include "Rails Bag"
+      expect(response.body).not_to include "22.99"
+    end
   end
 end
