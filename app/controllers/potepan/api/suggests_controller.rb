2cc12891f18833e060fb7b0d9cea8f21f20a9c59
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-05-04 10:12:38 +0900

    fix exec rubocop

--- app/controllers/potepan/api/suggests_controller.rb
+++ app/controllers/potepan/api/suggests_controller.rb
@@ -4,7 +4,7 @@ class Potepan::Api::SuggestsController < ApplicationController
     client = HTTPClient.new
     uri = 'https://presite-potepanec-task5.herokuapp.com/potepan/api/suggests'
     key = ENV["POTEPAN_API_KEY"]
-    query = {'keyword' => 'r', 'max_num' => 5}
+    query = { 'keyword' => 'r', 'max_num' => 5 }
     header = { 'Authorization' => "Bearer #{key}" }
     @sgst_res = client.get(uri, query: query, header: header).body
   end
