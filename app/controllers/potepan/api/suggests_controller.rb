21435ec620a4f409c28ee6a10722452d809b9a4e
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-05-07 00:32:12 +0900

    add error handling logic to suggests controller

--- app/controllers/potepan/api/suggests_controller.rb
+++ app/controllers/potepan/api/suggests_controller.rb
@@ -4,6 +4,24 @@ class Potepan::Api::SuggestsController < ApplicationController
     params[:keyword] = "r"
     params[:max_num] = 5
     con = Potepan::Request::SuggestsRequest.build(params)
-    @sgst_res = Potepan::APIRequest.send(con)
+    res = Potepan::APIRequest.send(con)
+    status = res.status
+
+    if status == 200
+      render status: status, json: { status: status, message: res.body }
+    else
+      case status
+      when 400
+        render status: status, json: { status: status, message: "Bad Request" }
+      when 401
+        render status: status, json: { status: status, message: "Unauthorized" }
+      when 404
+        render status: status, json: { status: status, message: "Not Found" }
+      when 500..599
+        render status: 500, json: { status: status, message: "Internal Server Error" }
+      else
+        render status: status, json: { status: status, message: "Sever Error" }
+      end
+    end
   end
 end
