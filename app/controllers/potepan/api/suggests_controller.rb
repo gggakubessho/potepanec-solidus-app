27c37e22fda818b79c103c2a3750db549b0af023
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-05-11 08:24:29 +0900

    fix base authenticate logic

--- app/controllers/potepan/api/suggests_controller.rb
+++ app/controllers/potepan/api/suggests_controller.rb
@@ -1,26 +1,21 @@
 class Potepan::Api::SuggestsController < ApplicationController
+  TOKEN = ENV["SUGGESTS_API_KEY"]
+  before_action :authenticate
   def index
-    api_params = {}
-    api_params[:keyword] = params[:keyword]
-    api_params[:max_num] = params[:max_num]
-    con = Potepan::Request::SuggestsRequest.build(api_params)
-    res = Potepan::APIRequest.send(con)
-    status = res.status
-    if status == 200
-      render status: status, json: res.body
-    else
-      case status
-      when 400
-        render status: status, json: { status: status, message: "Bad Request" }
-      when 401
-        render status: status, json: { status: status, message: "Unauthorized" }
-      when 404
-        render status: status, json: { status: status, message: "Not Found" }
-      when 500..599
-        render status: 500, json: { status: status, message: "Internal Server Error" }
-      else
-        render status: status, json: { status: status, message: "Sever Error" }
-      end
+  end
+
+  def authenticate
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      token == TOKEN
     end
   end
+
+  def render_unauthorized
+    render json: "unauthorized", status: :unauthorized
+  end
+
 end
