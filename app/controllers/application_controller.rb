8a66f1356f5b3f7ae673eb5dbbf0426beb07e12d
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-05-12 02:03:01 +0900

    replace statuscode to symbol

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ private
 def api_error_handler(status)
   case status
   when 400
-    render status: status, json: { status: status, message: "Bad Request" }
+    render status: :bad_request, json: { status: status, message: "Bad Request" }
   when 401
-    render status: status, json: { status: status, message: "Unauthorized" }
+    render status: :unauthorized, json: { status: status, message: "Unauthorized" }
   when 404
-    render status: status, json: { status: status, message: "Not Found" }
+    render status: :not_found, json: { status: status, message: "Not Found" }
   when 500..599
-    render status: 500, json: { status: status, message: "Internal Server Error" }
+    render status: :internal_server_error, json: { status: status, message: "Internal Server Error" }
   else
     render status: status, json: { status: status, message: "Sever Error" }
   end
