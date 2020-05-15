14e24b7bae1df18686bbbfb4da696855ed700606
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-05-16 07:32:51 +0900

    refactoring to use strong parameter in getting parameters

--- app/controllers/potepan/api/suggests_controller.rb
+++ app/controllers/potepan/api/suggests_controller.rb
@@ -1,9 +1,6 @@
 class Potepan::Api::SuggestsController < ApplicationController
   def index
-    api_params = {}
-    api_params[:keyword] = params[:keyword]
-    api_params[:max_num] = params[:max_num]
-    suggest_request = Potepan::Request::SuggestsRequest.build(api_params)
+    suggest_request = Potepan::Request::SuggestsRequest.build(suggest_params)
     res = suggest_request.send
     status = res.status
     if status == 200
@@ -11,5 +8,11 @@ class Potepan::Api::SuggestsController < ApplicationController
     else
       api_error_handler(status)
     end
+  end
+
+  private
+
+  def suggest_params
+    params.permit(:keyword, :max_num)
   end
 end
